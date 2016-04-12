21714b99d908041a52e7d9d152f3e31ecd5436a5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-04-12 18:32:31 -0400

    ENH: Update GenerateCLP tests to build/install its own GenerateCLP. See #44
    This will ensure running the tests will not delete files associated
    with the current CMAKE_INSTALL_PREFIX.
    
    Co-authored-by: Nicole Aucoin <nicole@bwh.harvard.edu>

--- GenerateCLP/Testing/CMake/GenerateCLP-Install.cmake
+++ GenerateCLP/Testing/CMake/GenerateCLP-Install.cmake
@@ -2,7 +2,16 @@
 # --------------------------------------------------------------------------
 # Sanity checks
 
-foreach(varname TEST_CMAKE_DIR TEST_BINARY_DIR TEST_INSTALL_DIR)
+foreach(varname
+    CMAKE_BUILD_TYPE
+    CMAKE_GENERATOR
+    ModuleDescriptionParser_DIR
+    TCLAP_DIR
+    TEST_CMAKE_DIR
+    TEST_SOURCE_DIR
+    TEST_BINARY_DIR
+    TEST_INSTALL_DIR
+  )
   if(NOT DEFINED ${varname})
     message(FATAL_ERROR "Variable ${varname} is not DEFINED")
   endif()
@@ -11,14 +20,16 @@ endforeach()
 include(${TEST_CMAKE_DIR}/GenerateCLPTestMacros.cmake)
 
 # --------------------------------------------------------------------------
-# Delete install directory if it exists
+# Delete build and install directory if they exists
 execute_process(
+  COMMAND ${CMAKE_COMMAND} -E remove_directory ${TEST_BINARY_DIR}
   COMMAND ${CMAKE_COMMAND} -E remove_directory ${TEST_INSTALL_DIR}
   )
 
 # --------------------------------------------------------------------------
-# Create install directory
+# Create build and install directories
 execute_process(
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${TEST_BINARY_DIR}
   COMMAND ${CMAKE_COMMAND} -E make_directory ${TEST_INSTALL_DIR}
   )
 
@@ -27,12 +38,50 @@ execute_process(
 set(PRINT_COMMAND 0)
 
 # --------------------------------------------------------------------------
+# Configure
+set(command ${CMAKE_COMMAND}
+  -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+  -DCMAKE_INSTALL_PREFIX:PATH=${TEST_INSTALL_DIR}
+  -DModuleDescriptionParser_DIR:PATH=${ModuleDescriptionParser_DIR}
+  -DTCLAP_DIR:PATH=${TCLAP_DIR}
+  -G ${CMAKE_GENERATOR} ${TEST_SOURCE_DIR})
+execute_process(
+  COMMAND ${command}
+  WORKING_DIRECTORY ${TEST_BINARY_DIR}
+  OUTPUT_VARIABLE ov
+  RESULT_VARIABLE rv
+  )
+
+print_command_as_string("${command}")
+
+if(rv)
+  message(FATAL_ERROR "Failed to configure Test:\n${ov}")
+endif()
+
+# --------------------------------------------------------------------------
+# Build
+
+set(command ${CMAKE_COMMAND} --build ${TEST_BINARY_DIR} --config ${CMAKE_BUILD_TYPE})
+execute_process(
+  COMMAND ${command}
+  WORKING_DIRECTORY ${TEST_BINARY_DIR}
+  OUTPUT_VARIABLE ov
+  RESULT_VARIABLE rv
+  )
+
+print_command_as_string("${command}")
+
+if(rv)
+  message(FATAL_ERROR "Failed to build Test:\n${ov}")
+endif()
+
+# --------------------------------------------------------------------------
 # Install
 set(install_target install)
 if(WIN32)
   set(install_target INSTALL)
 endif()
-set(command ${CMAKE_COMMAND} --build ${TEST_BINARY_DIR} --config Release --target ${install_target})
+set(command ${CMAKE_COMMAND} --build ${TEST_BINARY_DIR} --config ${CMAKE_BUILD_TYPE} --target ${install_target})
 execute_process(
   COMMAND ${command}
   WORKING_DIRECTORY ${TEST_BINARY_DIR}
