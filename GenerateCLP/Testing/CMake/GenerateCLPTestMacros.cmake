69ed4debf3193380554a45474d7637917a70e82d
Johan Andruejol <johan.andruejol@kitware.com>
2016-09-02 11:58:51 -0400

    STYLE: GenerateCLP: Factorize out the CMakeLists of the testing CLIs

--- GenerateCLP/Testing/CMake/GenerateCLPTestMacros.cmake
+++ GenerateCLP/Testing/CMake/GenerateCLPTestMacros.cmake
@@ -11,3 +11,51 @@ function(print_command_as_string command)
   endif()
 endfunction()
 
+# --------------------------------------------------------------------------
+# Macro used to generate CLIs to test GenerateCLP with
+macro(GenerateCLP_TEST_PROJECT)
+  include(CMakeParseArguments)
+
+  set(options)
+  set(oneValueArgs NAME)
+  set(multiValueArgs)
+  cmake_parse_arguments(TEST_CLI "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+  project(${TEST_CLI_NAME})
+
+  find_package(GenerateCLP NO_MODULE REQUIRED)
+  include(${GenerateCLP_USE_FILE})
+
+  #-----------------------------------------------------------------------------
+  if(GenerateCLP_USE_JSONCPP)
+    set(CMAKE_MODULE_PATH ${JsonCpp_CMAKE_MODULE_PATH} ${CMAKE_MODULE_PATH}) # Needed to locate FindJsonCpp.cmake
+    find_package(JsonCpp REQUIRED)
+    include_directories(${JsonCpp_INCLUDE_DIRS})
+  endif()
+
+  set(_additional_link_libraries)
+  if(GenerateCLP_USE_JSONCPP)
+    list(APPEND _additional_link_libraries ${JsonCpp_LIBRARIES})
+  endif()
+  if(GenerateCLP_USE_SERIALIZER)
+    list(APPEND _additional_link_libraries ${ParameterSerializer_LIBRARIES})
+  endif()
+
+  #-----------------------------------------------------------------------------
+  # Build
+  #-----------------------------------------------------------------------------
+
+  set(${PROJECT_NAME}_SOURCE ${PROJECT_NAME}.cxx)
+  GENERATECLP(${PROJECT_NAME}_SOURCE ${PROJECT_NAME}.xml)
+  add_executable(${PROJECT_NAME} ${${PROJECT_NAME}_SOURCE})
+  if(_additional_link_libraries)
+    target_link_libraries(${PROJECT_NAME} ${_additional_link_libraries})
+  endif()
+  #-----------------------------------------------------------------------------
+  # Test
+  #-----------------------------------------------------------------------------
+  include(CTest)
+
+  set(TEMP ${PROJECT_BINARY_DIR}/Testing/Temporary)
+  file(MAKE_DIRECTORY ${TEMP})
+endmacro()
