089eb5c1aeb42ba99528627db41041fa4a756504
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-28 00:31:37 -0400

    docker: Associate build-time metadata with the image

--- test/Docker/build.sh
+++ test/Docker/build.sh
@@ -15,5 +15,11 @@ then
   die "Empty Path to Dockerfile "
 fi
 
+image=slicer/slicerexecutionmodel
 lower_case_tag="`echo $1 | tr "[:upper:]" "[:lower:]" `"
-docker build -t slicer/slicerexecutionmodel:$lower_case_tag $2
+docker build -t $image:$lower_case_tag \
+  --build-arg IMAGE=$image \
+  --build-arg VCS_REF=`git rev-parse --short HEAD` \
+  --build-arg VCS_URL=`git config --get remote.origin.url` \
+  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+  $2
