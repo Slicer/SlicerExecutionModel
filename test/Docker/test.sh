d6830a4cd7b6ad143dca0fbcc763677598930a35
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-21 15:00:49 -0400

    test: Simplify scripts to ensure exit on first error

--- test/Docker/test.sh
+++ test/Docker/test.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
+set -ex
+
 # This is a script to build the project and run the test suite in the base
 # Docker container.
 
-die() {
-  echo "Error: $@" 1>&2
-  exit 1;
-}
-
-cd /usr/src/SlicerExecutionModel-build || die "Could not cd into the build directory"
-
+# SlicerExecutionModel
+mkdir -p /usr/src/SlicerExecutionModel-build && cd $_
 cmake \
   -G Ninja \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DBUILDNAME:STRING=$1 \
-    /usr/src/SlicerExecutionModel || die "CMake configuration failed"
-ctest -VV -D Experimental || die "ctest failed"
+    /usr/src/SlicerExecutionModel
+ctest -VV -D Experimental
