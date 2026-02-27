ae8b91f45550127595f1cbe3f7225098e9224574
Hans J. Johnson <hans-johnson@uiowa.edu>
2026-02-27 09:51:47 -0600

    ENH: Allow docker testing support scripts to work outside of docker
    Make the testing scripts more generic so that they can be run
    in a standard environment to improve debugging convenience.
    
    Replace hard-coded docker paths with auto-generated relative paths.
    Explicitly specify build and source directories for easier tracking.

--- test/Docker/test.sh
+++ test/Docker/test.sh
@@ -5,11 +5,19 @@ set -ex
 # This is a script to build the project and run the test suite in the base
 # Docker container.
 
+SEM_ROOT_DIR=$(git rev-parse --show-toplevel)
+SRC_ROOT_DIR=$(dirname ${SEM_ROOT_DIR})
+
+if [ -z "${ITK_DIR}" ]; then
+  echo "WARNING:  Environmental variable ITK_DIR must be set to support building SlicerExecutionModel"
+fi
+
 # SlicerExecutionModel
-mkdir -p /usr/src/SlicerExecutionModel-build && cd $_
+mkdir -p ${SRC_ROOT_DIR}/SlicerExecutionModel-build && cd $_
 cmake \
   -G Ninja \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DBUILDNAME:STRING=$1 \
-    /usr/src/SlicerExecutionModel
+  -B ${SRC_ROOT_DIR}/SlicerExecutionModel-build \
+  -S ${SEM_ROOT_DIR}
 ctest -VV -D Experimental
